034f12e8237d6f7ac67751ceb11c47ab4867e387
Drashtiie <dpate191@asu.edu>
2022-10-27 11:17:20 -0700

    Initial skeleton

---
+++ urinals.java
@@ -0,0 +1,27 @@
+
+//Author - Drashti Pzatel
+
+
+
+public class urinals {
+    public static String getString(){
+        System.out.println("Get Stirng");
+        return " ";
+    }
+    int countUrinals(){
+        System.out.println("Count Urinals");
+        return 0;
+    }
+    void openFile(){
+        System.out.println("Open File");
+
+    }
+    public static void main(String args[]){
+        urinals b = new urinals();
+        System.out.println("Main");
+//        String a = getString();
+        System.out.println(b.getString());
+
+
+    }
+}
