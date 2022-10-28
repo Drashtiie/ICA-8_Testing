3e16431b7cf47a7c946a52f7252759ad4e260d85
Drashtiie <dpate191@asu.edu>
2022-10-27 22:55:13 -0700

    Test 3 added

--- urinals.java
+++ urinals.java
@@ -35,8 +35,6 @@ public class urinals {
         return true;
     }
     int countUrinals(String a) throws IOException{
-        System.out.println("Count");
-//        int i = a.length();
         int j = 0;
         int c=0;
         int ans=0;
@@ -51,7 +49,6 @@ public class urinals {
         while(cv){
             String fname= "rule".concat(getString(p));
             String fname2 =fname.concat(".txt");
-            System.out.println(fname2);
             String p1 = "D:\\ICA-8_Testing\\".concat(fname2);
             File fileName = new File(p1);
             if(fileName.exists()){
@@ -61,16 +58,11 @@ public class urinals {
                 dfilename=p1;
                 cv=false;
             }
-
-
         }
-        System.out.println(dfilename);
-
         Path fileName = Path.of(
                 dfilename);
 
         if(!goodString(e1)){
-//            Files.writeString(fileName, "-1", StandardOpenOption.CREATE);
             this.ansarr.add("-1");
             return -1;
         }
@@ -103,17 +95,11 @@ public class urinals {
         return ans;
     }
     void openFile() throws IOException {
-        System.out.println("Open File");
         Path fileName
                 = Path.of("D:\\ICA-8_Testing\\urinal.dat");
-
-//        Scanner sc = new Scanner(fileName);
-
         try {
             Scanner sc = new Scanner(fileName);
-
             int i =0;
-
             while (sc.hasNext()) {
                 String b= sc.next();
                 System.out.println(countUrinals(b)+ " " + b );
@@ -121,16 +107,20 @@ public class urinals {
             sc.close();
         }
         catch (Exception e) {
-
-//            e.printStackTrace();
-//            sc.close();
-//            handling file not found exception
             System.out.println("File doesnot exists");
         }
-//        System.out.println("A");
         printanstofile();
     }
-
+    public static int isFileExists(File file) {
+        if (file.isFile()) {
+            System.out.println("File exists!!");
+            return 1;
+        }
+        else {
+            System.out.println("File doesn't exist");
+            return 0;
+        }
+    }
     public void printanstofile(){
         int p = 0;
         String dfilename="";
@@ -138,7 +128,6 @@ public class urinals {
         while(cv) {
             String fname = "rule".concat(getString(p));
             String fname2 = fname.concat(".txt");
-            System.out.println(fname2);
             String p1 = "D:\\ICA-8_Testing\\".concat(fname2);
             File fileName = new File(p1);
             if (fileName.exists()) {
@@ -148,35 +137,22 @@ public class urinals {
                 cv = false;
             }
         }
-        System.out.println(dfilename);
-
         Path fileName = Path.of(
                 dfilename);
-
-//        ArrayList<Integer> ansarr2=this.ansarr;
         try {
             Files.write(fileName, this.ansarr);
-            System.out.println("Written");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
     public static void main(String args[]) throws IOException {
         urinals b = new urinals();
-        System.out.println("Main");
-//        String a = getString();
-//        System.out.println(b.getString());
         b.openFile();
-//        b.countUrinals();
-
         Result result = JUnitCore.runClasses(urinalsTest.class);
 
         for (Failure fail : result.getFailures()) {
             System.out.println(fail.toString());
         }
-
         System.out.println(result.wasSuccessful());
     }
-
-
 }
--- urinalsTest.java
+++ urinalsTest.java
@@ -1,30 +1,56 @@
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.io.File;
 
 import static org.junit.Assert.*;
+
 public class urinalsTest {
     urinals t =new urinals();
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
     @Test
-    public void checkGoodString(){
+    public void fileReadTest()  {
         System.out.println("====== Drashti Patel == TEST ONE EXECUTED =======");
-
-        System.out.println("====== Drashti Patel == TEST TWO  EXECUTED =======");
         File fileName = new File("D:\\ICA-8_Testing\\urinal.dat");
-
         try {
             assertTrue(fileName.exists());
         }
         catch (Exception e) {
-           System.out.println("File doesnot exists");
+            System.out.println("File exists");
             assertFalse(fileName.exists());
         }
+        File fileName2 = new File("D:\\ICA-8_Testing\\urinal2.dat");
+        try {
+            assertFalse(fileName2.exists());
+        }
+        catch (Exception e) {
+            System.out.println("File doesnot exists");
+            assertFalse(fileName2.exists());
+        }
+
+
+
+    }
+    @Test
+    public void isFileExistsTest() {
+        assertEquals(1, t.isFileExists(new File("D:\\ICA-8_Testing\\urinal.dat")));
+
+        assertEquals(0, t.isFileExists(new File("D:\\ICA-8_Testing\\urinal1.dat")));
+    }
+    @Test
+    public void checkGoodString(){
+        System.out.println("====== Drashti Patel == TEST TWO  EXECUTED =======");
+
 //
         System.out.println("====== Drashti Patel == TEST THREE EXECUTED =======");
 
         assertEquals("0000 is good string",true, t.goodString("0000"));
-        assertEquals("0000 is good string",false, t.goodString("Drashti"));
-        assertEquals("011 is  good string",false, t.goodString("011"));
+        assertEquals("Drashti is not good string",false, t.goodString("Drashti"));
+        assertEquals("011 is not a good string",false, t.goodString("011"));
+
+        assertEquals("0102 is not a good string",false, t.goodString("0102"));
 
     }
 
